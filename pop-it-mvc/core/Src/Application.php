d3375238471963b33bbf623a11d992577ec50b52
gr434_shaaal2 <arseniysharnin@gmail.com>
2026-03-16 11:44:23 +0700

    first

---
+++ pop-it-mvc/core/Src/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Src;
+
+class Application
+{
+    private Settings $settings;
+
+    public function __construct(Settings $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function __get($key)
+    {
+        if ($key === 'settings') {
+            return $this->settings;
+        }
+        throw new Error('Accessing a non-existent property');
+    }
+
+    public function run(): void
+    {
+        echo 'Working';
+    }
+}
